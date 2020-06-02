1daeefe55e0969ace3cf40c90128b0284858d61a
vivolric <62187112+vivolric@users.noreply.github.com>
2020-06-01 23:03:55 -0500

    update1

---
+++ src/Package1/Class1.java
@@ -0,0 +1,9 @@
+package Package1;
+
+public class Class1 {
+    public static void main(String[] args) {
+
+        System.out.println("Count your age by friends, not years.");
+        System.out.println("Count your life by smiles, not tears.");
+    }
+}
---
+++ src/Package1/gitHubCommands.java
@@ -0,0 +1,29 @@
+package Package1;
+
+public class gitHubCommands {
+    /*
+    cd ../..   --> delete and go back to user
+    cd         --> change directory
+    ls         --> list (mac)
+    dir        --> directory (windows, linux)
+    clear      --> delete everything
+    git status --> which files has changed or created
+    git add *   --> adding the files which you want to commit
+                   * (star) is adding all files
+                   bu ayrinti muhim albayim, * ile add arasinda bi space var
+
+    git commit - m 'your message' --> committing the changes for the
+                                      pushing to code to github
+
+    git push      --> pushing the code to github
+    git branch    --> which branches are available
+
+
+
+
+
+
+
+
+     */
+}
